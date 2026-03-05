ac8026ba507ee386aae7f4d007315eccf001e180
Christopher Bunting <christophersbunting@gmail.com>
2026-03-04 22:00:49 -0800

    Initial Commit

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Christopher Bunting on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeUsageTests/ClaudeUsageTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageTests.swift
+//  ClaudeUsageTests
+//
+//  Created by Christopher Bunting on 3/4/26.
+//
+
+import Testing
+@testable import ClaudeUsage
+
+struct ClaudeUsageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
